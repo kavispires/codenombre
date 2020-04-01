bf1098d60392c1e983a1f827a7a74deae64cfe70
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2020-04-01 16:20:01 -0700

    Updates

---
+++ precache-manifest.8d3220c3d5632f5e9a0eba461a0542ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4063a3945c5bc19e4ea70be3211332",
+    "url": "/codenombre/index.html"
+  },
+  {
+    "revision": "6933fe3daa8c20c1e16b",
+    "url": "/codenombre/static/css/main.2875a583.chunk.css"
+  },
+  {
+    "revision": "e6e2087642e93ea5e26f",
+    "url": "/codenombre/static/js/2.639c84ce.chunk.js"
+  },
+  {
+    "revision": "6446e164477ddd3a9ff9cb9a99a39110",
+    "url": "/codenombre/static/js/2.639c84ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6933fe3daa8c20c1e16b",
+    "url": "/codenombre/static/js/main.84c5cdea.chunk.js"
+  },
+  {
+    "revision": "3511a2eaefe076489ba1",
+    "url": "/codenombre/static/js/runtime-main.02d9c10e.js"
+  }
+]);
